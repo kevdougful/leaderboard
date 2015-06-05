14f8a77ae63954b5fddb377b33bd435c2b225306
Kevin D Coffey <kevdougful@gmail.com>
2015-06-05 13:56:59 -0500

    added database submit page

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// MongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/Leaders');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -22,6 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,48 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    var db = req.db;
+    var teams = db.get('Teams');
+    teams.find({}, {}, function (e, docs) {
+        res.render('index', {
+            "teamlist" : docs
+        });
+    });
+    //res.render('index', { title: 'Express' });
+});
+
+/* ADD Team */
+router.get('/addteam', function (req, res) {
+    var db = req.db;
+    var teams = db.get('Teams');
+    teams.find({}, {}, function (e, docs) {
+        res.render('addteam', {
+            "teamlist" : docs
+        });
+    });
+});
+router.post('/addteam', function (req, res) {
+    var db = req.db;
+
+    var captain = req.body.teamcaptain;
+    var number = req.body.teamnumber;
+    var score = req.body.teamscore;
+
+    var teams = db.get('Teams');
+
+    teams.insert({
+        "Team_Captain" : captain,
+        "Team_Number" : number,
+        "Team_Score" : score
+    }, function (err, doc) {
+        if (err) {
+            res.send("Error sending to database");
+        }
+        else {
+            res.location("/addteam");
+            res.redirect("/addteam");
+        }
+    });
 });
 
 module.exports = router;
