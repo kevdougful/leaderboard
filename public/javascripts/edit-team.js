8003630d9be30ea8da278e6178882b85f16dadce
Kevin Coffey <kevdougful@gmail.com>
2015-06-07 00:48:04 -0500

    edit-team now populates all modal fields

--- public/javascripts/edit-team.js
+++ public/javascripts/edit-team.js
@@ -1,4 +1,8 @@
 $(document).on("click", ".open-editteam", function() {
-	var teamnum = $(this).data('id');
+	var teamnum = $(this).data('number');
     $(".modal-body #teamnumber").val(teamnum);
+	var teamcap = $(this).data('captain');
+    $(".modal-body #teamcaptain").val(teamcap);
+	var teamsco = $(this).data('score');
+    $(".modal-body #teamscore").val(teamsco);
 });
