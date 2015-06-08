c7bd0a5d6808cff91ec41b9f8f281867a5fa6230
unknown <kdcoff1@R9WEH7R.na.ds.monsanto.com>
2015-06-08 15:00:06 -0500

    implemented numerical and ordinal place numbering

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,47 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+function setPlaces(topten) {
+    var place = 1;
+    for (var i = 0; i < topten.length; i++) {
+        if (i == 0) {
+            // First team, so set it to 1st
+            topten[i]["Place"] = Number(place);
+            topten[i]["Ordinal"] = ordinal(place);
+            continue;
+        } else {
+            if (topten[i]["Team_Score"] < topten[i - 1]["Team_Score"]) {
+                // Not tied with previous team
+                topten[i]["Place"] = Number(++place);
+                topten[i]["Ordinal"] = ordinal(place);
+            } else {
+                // Tied with previous team
+                topten[i]["Place"] = Number(place);
+                topten[i]["Ordinal"] = ordinal(place);
+            }
+        }
+    }
+    return JSON.parse(topten);
+}
+
+function ordinal(n) {
+	var s = ["th", "st", "nd", "rd"],
+		v = n % 100;
+	return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+/* Raw JSON page */
+router.get('/raw', function(req, res) {
+    var db = req.db;
+    var teams = db.get('Teams');
+    teams.find({}, { "sort" : [['Team_Score','desc']] }, function (e, docs) {
+        docs = setPlaces(docs);
+        res.render('raw', {
+            "rawjson" : JSON.stringify(docs, null, 2),
+        });
+    });
+});
+
 /* GET home page. */
 router.get('/', function (req, res) {
     var db = req.db;
